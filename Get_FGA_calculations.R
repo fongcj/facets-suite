37d78bfd451792339489efaab5c925ef9fcf39ef
Chris Fong <fongc2@mskcc.org>
2019-03-05 12:19:30 -0500

    - Adding facets_folder_struct.py Script for obtaining file/folder locations for a given list of sample ids
    - Adding Get_FGA_calculations.R
    Script for extracting FGA calculations from Rdata files

---
+++ Get_FGA_calculations.R
@@ -0,0 +1,54 @@
+###########################################################################
+# Get_FGA_calculations
+#
+#
+# By Chris Fong, MSKCC 2018
+#
+###########################################################################
+if("data.table" %in% rownames(installed.packages()) == FALSE) {install.packages("data.table")}
+# if("facetsSuite" %in% rownames(installed.packages()) == FALSE) {install.packages("facetsSuite")}
+if("usethis" %in% rownames(installed.packages()) == FALSE) {install.packages("usethis")}
+if("readr" %in% rownames(installed.packages()) == FALSE) {install.packages("readr")}
+
+library("data.table")
+library("dplyr")
+library("methods")
+# library("facetsSuite")
+
+# Source facets functions
+print('Loading facets suite')
+source("R/copy-number-scores.R")
+source("data-raw/sysdata.R")
+
+print('Loading facets locations')
+pathname = "data"
+fname = 'facets_file_locations_AACR_2019.csv'
+filenamePath <- file.path(pathname, fname)
+df_facets_loc <- read.csv(filenamePath, header = TRUE, sep = '\t')
+
+path_facets_loc <- '/ifs/res/taylorlab/impact_facets/'
+
+# Compute Fraction CNA
+print('Calculating Fraction of Genome Altered')
+for (i in 1:nrow(df_facets_loc)) {
+  # Build Pathname
+  folder_sample <- df$FOLDER_NAME_TOP[i]
+  folder_facets <- df$FOLDER_NAME_FACETS[i]
+  filename_facets <- df$FILENAME[i]
+  pathfilename <- file.path(path_facets_loc, folder_sample, folder_facets, filename_facets)
+  # Load R data
+  load(pathfilename)
+  
+  # Compute CNA
+  facets_cna <- calculate_fraction_cna(segs=fit$cncf, ploidy=fit$ploidy, "hg19", "em")
+  
+  # Place into df
+  gd <- fraction_cna$genome_doubled
+  fga <- fraction_cna$fraction_cna
+  df_facets_loc$genome_doubled[i] <- gd
+  df_facets_loc$fraction_cna[i] <- fga
+}
+
+
+write.csv(df_facets_loc, file = "facets_fga_AACR_2019.csv")
+
---
+++ facets_folder_struct.py
@@ -0,0 +1,78 @@
+"""
+extract_facets_from_luna.py
+
+By Chris Fong - MSKCC 2019
+
+
+"""
+import os
+import pandas as pd
+import numpy as np
+from shutil import copyfile
+
+
+desired_width = 320
+pd.set_option('display.width', desired_width)
+np.set_printoptions(linewidth=desired_width)
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+pd.set_option('display.expand_frame_repr', False)
+
+# Data type selection
+dtype_suffix = '.Rdata'
+
+# Read sample list file
+path = '/Users/fongc2/Documents/github/MSK/metastatic_tropisms/data/AACR_2019'
+fname = 'sample_ids_aacr_2019.csv'
+pathfilename = os.path.join(path, fname)
+sample_ids = pd.read_csv(pathfilename, header=0)
+
+# Read directory names, or determine directory list using os.listdir
+# Mount example:
+path_root = '/ifs/res/taylorlab/impact_facets/all/'         # Path for mounted Luna drive
+fname_folder_names = 'facet_folder_names_chai.csv'
+path_folder_names = 'data'
+pathfilename_folders = os.path.join(path_folder_names, fname_folder_names)
+df_folder_names = pd.read_csv(pathfilename_folders, header=0, low_memory=False)
+# folder_names = os.listdir(path_root)
+# df_folder_names = pd.DataFrame(folder_names, columns=['subfolder'])
+# df_folder_names.to_csv(fname_folder_names, index=False)
+
+
+# Create data frame containing path and file info, parameters, etc
+cols_df = ['SAMPLE_ID', 'FOLDER_NAME_TOP', 'FOLDER_NAME_FACETS', 'FILENAME']
+df = pd.DataFrame(columns=cols_df)
+df.head()
+
+# Convert foldername df to list
+folder_names = list(df_folder_names['subfolder'])
+ctr = 0
+
+for i, current_sample_id in enumerate(list(list(sample_ids.iloc[:, 0]))):
+    matching = [s for s in folder_names if current_sample_id in s]
+    for m, current_sample_dir in enumerate(matching):
+        # Get list of dir from this folder
+        path1 = os.path.join(path_root, current_sample_dir)
+        try:
+            folder_name1 = os.listdir(path1)
+        except:
+
+        matching1 = [s for s in folder_name1 if 'facets' in s]
+        for j, current_facet_dir in enumerate(matching1):
+            path2 = os.path.join(path1, current_facet_dir)
+            folder_name2 = os.listdir(path2)
+            matching2 = [s for s in folder_name2 if dtype_suffix in s]
+
+            for k, current_rdata in enumerate(matching2):
+                # Build rows in dataframe here
+                df.at[ctr] = [current_sample_id, current_sample_dir, current_facet_dir, current_rdata]
+                ctr += 1
+
+
+df.to_csv('facets_file_locations_AACR_2019.csv', index=False)
+
+tmp = 0
+
+
+
